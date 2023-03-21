7768e38105b45ae15884249bf5b6bb384303129e
Ilya <xcaz@inbox.ru>
2023-03-21 21:23:49 +0400

    Client.2

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -5,9 +5,17 @@ import com.example.grpc.GreetingServiceOuterClass;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import javax.swing.*;
+import java.awt.event.WindowEvent;
 import java.util.Iterator;
 
-public class Client {
+class Client {
+    static JFrame window = new JFrame();
+
+    static JLabel screen = new JLabel();
+
+    static ImageIcon ic;
+
     public static void main(String[] args) {
         ManagedChannel managedChannel = ManagedChannelBuilder.forTarget("localhost:8080")
                 .usePlaintext().build();
@@ -15,8 +23,21 @@ public class Client {
         GreetingServiceOuterClass.MsgRequest request = GreetingServiceOuterClass.MsgRequest.newBuilder()
                 .build();
         Iterator<GreetingServiceOuterClass.MsgResponse> response = stub.greeting(request);
-        while (response.hasNext())
-            System.out.println(response.next());
+        playing(response);
         managedChannel.shutdownNow();
+    }
+
+    public static void playing(Iterator<GreetingServiceOuterClass.MsgResponse> response) {
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+        while (response.hasNext()) {
+            byte[] bytes = response.next().getGreeting().toByteArray();
+            ic = new ImageIcon(bytes);
+            screen.setIcon(ic);
+            window.setContentPane(screen);
+            window.pack();
+        }
+        window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
     }
 }
