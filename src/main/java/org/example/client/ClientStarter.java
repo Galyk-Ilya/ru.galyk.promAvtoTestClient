b1839050b9c4ab9880b177b582a367e169d7cd02
Ilya <xcaz@inbox.ru>
2023-03-22 21:07:27 +0400

    Client.3

---
+++ src/main/java/org/example/client/ClientStarter.java
@@ -0,0 +1,11 @@
+package org.example.client;
+
+import org.example.client.service.VideoService;
+
+class ClientStarter {
+
+    public static void main(String[] args) {
+        VideoService service = new VideoService("localhost:8080", 1);
+        service.start();
+    }
+}
--- src/main/java/org/example/client/service/VideoService.java
+++ src/main/java/org/example/client/service/VideoService.java
@@ -1,37 +1,50 @@
-package org.example;
+package org.example.client.service;
 
 import com.example.grpc.GreetingServiceGrpc;
 import com.example.grpc.GreetingServiceOuterClass;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.swing.*;
 import java.awt.event.WindowEvent;
 import java.util.Iterator;
 
-class Client {
-    static JFrame window = new JFrame();
-    static JLabel screen = new JLabel();
-    static ImageIcon ic;
+@Slf4j
+public class VideoService {
 
-    public static void main(String[] args) {
-        ManagedChannel managedChannel = ManagedChannelBuilder.forTarget("localhost:8080")
+    private static final JFrame window = new JFrame();
+    private static final JLabel screen = new JLabel();
+
+    private final String address;
+
+    private final int compress;
+
+    public VideoService(String address, int compress) {
+        this.address = address;
+        this.compress = compress;
+    }
+
+    public void start() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forTarget(address)
                 .usePlaintext().build();
         GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(managedChannel);
         GreetingServiceOuterClass.MsgRequest request = GreetingServiceOuterClass.MsgRequest.newBuilder()
+                .setCompressLvl(compress)
                 .build();
         Iterator<GreetingServiceOuterClass.MsgResponse> response = stub.greeting(request);
+        log.info("Server connection established");
         playing(response);
+        log.info("Server connection shutdown");
         managedChannel.shutdownNow();
     }
 
-    public static void playing(Iterator<GreetingServiceOuterClass.MsgResponse> response) {
-
+    private void playing(Iterator<GreetingServiceOuterClass.MsgResponse> response) {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
         while (response.hasNext()) {
             byte[] bytes = response.next().getGreeting().toByteArray();
-            ic = new ImageIcon(bytes);
+            ImageIcon ic = new ImageIcon(bytes);
             screen.setIcon(ic);
             window.setContentPane(screen);
             window.pack();
